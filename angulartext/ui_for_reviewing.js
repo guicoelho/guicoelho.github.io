e955876bf63de86d957bdcba07e9532f064a69c5
Guilherme Coelho <me@iamguico.com>
2014-09-02 14:43:25 +0200

    adding mockup ui

---
+++ angulartext/ui_for_reviewing.js
@@ -0,0 +1,63 @@
+// IIFE to ensure safe use of $
+(function( $ ) {
+  window.initializeTooltips = function() {
+    var $tooltip = $('<div class="tooltip">Accept/Reject change</div>').appendTo($('.container'));
+    window.$changes = $('.change');
+    var focusOn = -1;
+
+    var setFocus = function(index, changes){
+      var pos = window.$changes.eq(index).position();
+      $tooltip.css({
+        top: pos.top - $tooltip.outerHeight() - 20,
+        left: pos.left,
+        opacity: 1
+      });
+    }
+
+    var focusPrevious = function(changes){
+      window.$changes = $('.change');
+      if(focusOn > 0) {
+        focusOn -= 1;        
+      }
+      setFocus(focusOn);
+    }
+
+    var focusNext = function(changes){
+      window.$changes = $('.change');
+      if(focusOn < window.$changes.length - 1) {
+        focusOn += 1;        
+      }      
+      setFocus(focusOn);
+    }        
+
+    if($changes.length) {
+      setFocus(focusOn,$changes);
+    }
+    // Attach keyboard controls
+    document.onkeydown = checkKey;
+
+    function checkKey(e) {
+
+        // Prevent default behavior
+        e.stopPropagation();
+        e.preventDefault();  
+        e.returnValue = false;
+        e.cancelBubble = true;
+
+        e = e || window.event;
+
+        if (e.keyCode == '38') {
+            focusPrevious();
+        }
+        else if (e.keyCode == '40') {
+            focusNext();
+        }
+    }    
+
+  }
+
+window.hideTooltips = function() {
+  $('.tooltip').hide();
+}
+
+})(jQuery);
